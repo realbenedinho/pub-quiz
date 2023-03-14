85032210523771c5760eac1d4f494bc3ac7e60a6
Benedinho91 <bma@elegov.digital>
2023-03-14 21:21:13 +0100

    Carousel & Speicherfunktion hinzugefügt

--- pub-quiz/src/App.js
+++ pub-quiz/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
+import JSZip from 'jszip';
 
 function App() {
   const [image, setImage] = useState(null);
@@ -24,7 +25,7 @@ function App() {
         const pixelatedImages = [];
 
         for (let i = 1; i <= 35; i++) {
-          const pixelSize = i * 7;
+          const pixelSize = i * 10;
           const pixelatedCanvas = document.createElement('canvas');
           pixelatedCanvas.width = img.width;
           pixelatedCanvas.height = img.height;
@@ -52,23 +53,49 @@ function App() {
     };
     img.src = pixelatedImages[index];
   };
+ 
+  const handleSaveAllImages = () => {
+    const zip = new JSZip();
+
+    pixelatedImages.forEach((dataURL, index) => {
+      // Extract the image data from the base64-encoded data URL
+      const imageData = dataURL.split(',')[1];
+      // Add the image to the zip file
+      zip.file(`Pixelated${index}.png`, imageData, { base64: true });
+    });
+
+    // Generate the zip file and initiate a download
+    zip.generateAsync({ type: 'blob' }).then((content) => {
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(content);
+      link.download = 'pixelated_images.zip';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  };
+
 
   return (
     <div>
       <input type="file" onChange={handleImageUpload} />
       <br></br>
-      <Carousel fade>
+      <div className="carousel-container d-flex justify-content-center">
+      <Carousel fade data-interval="1" data-ride="true">
         {pixelatedImages.map((dataURL, index) => (
           <Carousel.Item key={index} onClick={() => handleImageSelection(index)}>
-            <img src={dataURL} className="d-block w-100 img-class" alt={"Pixelated " + index} />
+            <img src={dataURL} className="d-block img-class" alt={"Pixelated " + index} />
           </Carousel.Item>
         ))}
         {image && (
           <Carousel.Item>
-            <img src={image.src} className="d-block w-100 img-class" alt="Pixelated" />
+            <img src={image.src} className="d-block img-class" alt="Pixelated" />
           </Carousel.Item>
         )}
       </Carousel>
+      </div>
+      <br></br>
+      <button onClick={handleSaveAllImages}>Alle Bilder speichern</button>
     </div>
   );
 }
